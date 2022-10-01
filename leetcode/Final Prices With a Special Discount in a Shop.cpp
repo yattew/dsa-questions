4eafa45f05810b4eafe350ab36ea2ff1c91ec830
yattew <yathansh@gmail.com>
2022-10-01 16:26:15 +0530

    Final Prices With a Special Discount in a Shop

---
+++ leetcode/Final Prices With a Special Discount in a Shop.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/description/
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        for(int i = 0; i< prices.size()-1; i++)
+        {
+            for(int j = i+1; j<prices.size(); j++)
+            {
+                if(prices[j]<=prices[i])
+                {
+                    prices[i]-=prices[j];
+                    break;
+                }
+            }
+        }
+        return prices;
+    }
+};
